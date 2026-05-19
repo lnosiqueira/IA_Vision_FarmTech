90664bd42937a2851b0fd358001db377d1b04a1d
lnosiqueira <lnosiqueira@gmail.com>
2026-05-19 15:33:56 -0300

    fix: corrigindo indentação API

--- api/api.py
+++ api/api.py
@@ -4,33 +4,23 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"status": "online", "message": "API IA Vision FarmTech rodando 🚀"}
-
-@app.get("/health")
-def health():
-    return {"status": "ok"}
-
-@app.get("/alerta")
-def alerta():
-    return {
-        "alerta": "Temperatura elevada detectada!",
-        "nivel": "alto"
-    }
+    return {"status": "online", "message": "API IA Vision FarmTech"}
 
 @app.get("/analise")
 def analise(temp: float, umidade: float):
 
     if temp > 40:
-    risco = "CRÍTICO 🚨"
-    
-    if temp > 35 and umidade < 30:
+        risco = "CRÍTICO 🚨"
+        recomendacao = "Ação urgente na plantação"
+
+    elif temp > 35 and umidade < 30:
         risco = "ALTO 🔥"
         recomendacao = "Irrigar imediatamente"
-    
+
     elif temp > 30:
         risco = "MÉDIO ⚠️"
         recomendacao = "Monitorar irrigação"
-    
+
     else:
         risco = "BAIXO ✅"
         recomendacao = "Condições ideais"
